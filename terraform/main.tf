f99238f53f9bed9bad0802f51ef699636ebd1aa8
James Patrick <james@greenfiling.com>
2024-01-02 16:44:24 -0500

    Adding github provider info

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,18 @@
+variable "github_token" {
+  type        = string
+  description = "Your Github OAuth token"
+}
+
+variable "github_owner" {
+  type        = string
+  description = "The owner of the Github repository"
+}
+
+variable "github_name" {
+  type        = string
+  description = "The name of the Github repository"
+}
+
 terraform {
   required_providers {
     github = {
@@ -5,4 +20,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+}
+
+provider "github" {
+  token = var.github_token
+  owner = var.github_owner
+}
+
+resource "github_repository" "repository" {
+  name       = var.github_name
+  visibility = "private"
 }
